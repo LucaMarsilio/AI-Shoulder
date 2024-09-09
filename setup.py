72ef26426ced116494045075b72a734fb93db20f
LucaMarsilio <luca.marsilio@polimi.it>
2024-09-09 19:15:53 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Luca Marsilio",
+    author_email='luca.marsilio@polimi.it',
+    python_requires='>=3.7',
+    name='SegMentor',
+    version='0.1.0',
+    url='https://github.com/LucaMarsilio/SegMentor.git',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy"
+    ]
+)
